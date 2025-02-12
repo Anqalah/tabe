88d8514ce4f65623bbbdff755ee6e99fceed800f
anqalah <evodark7@gmail.com>
2025-02-12 23:17:10 +0800

    tada BE

--- fe/src/Pages/Attendances.jsx
+++
@@ -1,8 +0,0 @@
-// import ClockIn from "./Student/ClockIn";
-// import ClockOut from "./Student/ClockOut";
-
-// const AttendancePage = () => {
-//   return (<ClockIn />), (<ClockOut />);
-// };
-
-// export default AttendancePage;
